0e62aad40f7524c4032fb21b651f5c0a5151d83f
kimuati-codes <allankimutaikoech@gmail.com>
2024-03-22 16:47:56 +0300

    stash

--- index.ts
+++ index.ts
@@ -66,6 +66,32 @@ require("dotenv").config();
     }
 
     console.log("Navigation to child pages successful.");
+
+    // Handle login popup
+    await page.waitForSelector('a[href="/login.html"]');
+    await page.click('a[href="/login.html"]');
+
+    // Wait for the login popup to appear
+    await page.waitForSelector(".b-auth-popup");
+
+    // Click the "Sign in via email or phone" button
+    await page.click(
+      ".h-width-100p.h-bold.fw-button.qa-fw-button.fw-button--type-success.fw-button--size-large",
+    );
+
+    // Wait for the email or phone input field to appear
+    await page.waitForSelector(".qa-login-field");
+
+    // Fill in the email or phone input field
+    await page.type(".qa-login-field", `${process.env.NAME}`);
+
+    // Fill in the password input field
+    await page.type(".qa-password-field", `${process.env.PASSWORD}`);
+
+    // Click the "SIGN IN" button
+    await page.click(".qa-login-submit");
+    // Wait for login to complete
+    await page.waitForNavigation();
   } catch (error) {
     console.error("Error occurred during scraping process:", error);
   } finally {
